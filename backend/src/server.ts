61af934746f05ac1b95cad7f00a1adf2c896818f
Rafael Cena <rafael.cena@hotmail.com>
2020-10-13 13:51:46 -0300

    conexãoc om banco e migration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,10 @@
 import express from 'express'
 
+//importacao da conexão com o db
+import './database/connection'
 const app = express()
+
+app.use(express.json())
 
 app.get('/users', (request,response) => {
    return response.json({message:"Hello world"})
