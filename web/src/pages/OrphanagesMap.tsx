9987f519aee80f4fc4ad1f3f2867499d46e4b11d
Rafael Cena <rafael.cena@hotmail.com>
2020-10-16 13:05:15 -0300

    Começo da integração Front/back

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
-import { FiPlus } from 'react-icons/fi';
+import React, {useEffect, useState} from 'react';
+import { FiArrowRight, FiPlus } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer,Marker,Popup } from 'react-leaflet';
 
-import 'leaflet/dist/leaflet.css';
-
+import mapIcon from '../utils/mapIcon'
 import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
+import api from '../services/api';
+
+interface Orphanage{
+  id: number;
+  latitude:number;
+  longitude:number;
+  name: string;
+}
 
 const OrphanagesMap: React.FC = () => {
+  const [orphanages, setOrphanages] = useState([]);
+  useEffect(()=>{
+    api.get('orphanages').then(response =>{
+      setOrphanages(response.data)
+    })
+  },[])
+
   return (
     <div id="page-map">
       <aside>
@@ -32,10 +46,28 @@ const OrphanagesMap: React.FC = () => {
         style={{ width: '100%', height: '100%' }}
       >
         <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+    {orphanages.map(orphanage =>{
+      return(
+        <Marker
+        icon={mapIcon}
+        position={[orphanage.latitude,orphanage.lonetudi]}
+        >
+          <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup" >
+         
+            Lar das crianças
+            <Link to="/orphanages/1">
+              <FiArrowRight size={20} color="#fff"/>
+            </Link>
+
+          </Popup>
+      
+      </Marker>
+      )
+    })}
       </Map>
 
 
-      <Link to="" className="create-orphanage">
+      <Link to="/orphanage/create" className="create-orphanage">
         <FiPlus size={32} color="#FFF" />
       </Link>
     </div>
